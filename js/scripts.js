3e65764357805715f4bf4b7169483a4064cb8e1f
hmcvay <hayleymcvay8@gmail.com>
2022-01-10 13:57:40 -0800

    Add code to script for converting measurements.
    co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// function liter(number) {
+//   return number * 3.78541;
+// }
+
+// // const number = parseInt (prompt ("Enter Gallons:"));
+// // const result =liter(number);
+// alert(result);
+
+// function gal(number) {
+//   return number / 3.78541;
+// }
+// const number = parseInt (prompt("Enter Liters"));
+// const result = gal(number);
+// alert(result);
+
+// function ml(number) {
+//   return number * 29.5735;
+// }
+
+// const number = parseInt (prompt ("Enter oz:"));
+// const result = ml(number);
+// alert(result);
+
+function oz(number) {
+  return number / 29.5735;
+}
+
+const number = parseInt (prompt ("Enter ml:"));
+const result = oz(number);
+alert(result);
