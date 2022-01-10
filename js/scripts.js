97c755ee8d915398f4770b6e1184993709eccac9
hmcvay <hayleymcvay8@gmail.com>
2022-01-10 15:19:47 -0800

    Add additional conversions to scripts.js
    co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//gallons to liters
 // function liter(number) {
 //   return number * 3.78541;
 // }
@@ -6,6 +7,7 @@
 // // const result =liter(number);
 // alert(result);
 
+//liters to gallons
 // function gal(number) {
 //   return number / 3.78541;
 // }
@@ -13,6 +15,7 @@
 // const result = gal(number);
 // alert(result);
 
+//oz to ml
 // function ml(number) {
 //   return number * 29.5735;
 // }
@@ -21,10 +24,43 @@
 // const result = ml(number);
 // alert(result);
 
-function oz(number) {
-  return number / 29.5735;
-}
+//ml to oz
+// function oz(number) {
+//   return number / 29.5735;
+// }
 
-const number = parseInt (prompt ("Enter ml:"));
-const result = oz(number);
-alert(result);
+// const number = parseInt (prompt ("Enter ml:"));
+// const result = oz(number);
+// alert(result);
+
+// tsp to tbsp
+// function tbsp(number) {
+//   return number /3;
+// }
+// const number = parseInt (prompt("enter tsp"));
+// const result = tbsp(number);
+// alert (result);
+
+//tbsp to tsp
+// function tsp(number) {
+//   return number *3;
+// }
+// const number = parseInt (prompt ("enter tbsp"));
+// const result = tsp(number);
+// alert (result);
+
+//qts to gallons 
+// function gallons(number) {
+//   return number / 4;
+// }
+// const number = parseInt(prompt("enter qts"));
+// const result = gallons(number);
+// alert(result);
+
+//gallons to qts
+function qts(number) {
+  return number * 4;
+}
+const number = parseInt (prompt ("enter gallons:"));
+const result = qts(number);
+alert (result);
